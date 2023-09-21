ba049b5f71107095048a087215e18842178e1013
Keshav Agarwal <keshav1105ag@gmail.com>
2023-09-21 19:58:42 +0530

    completed basic java problems assignment

--- 06. Conditionals and Loops/Assignments/BasicJavaPrograms.java
+++ 06. Conditionals and Loops/Assignments/BasicJavaPrograms.java
@@ -9,7 +9,7 @@ public class BasicJavaPrograms {
        1. Area Of Circle Java Program
         System.out.print("Enter the radius of the circle (in cm): ");
         float r = input.nextFloat();
-        System.out.println("The area of the circle of radius "+r+" cm = "+(Math.round(3.14*r))+" sq. cm");
+        System.out.println("The area of the circle of radius "+r+" cm = "+(Math.round(3.14*r*r))+" sq. cm");
 */
 /*
        2. Area Of Triangle
@@ -52,29 +52,215 @@ public class BasicJavaPrograms {
 
 */
 /*
+       7. Area Of Equilateral Triangle
+        System.out.print("Enter the side length of equilateral triangle (in cm): ");
+        float side = input.nextFloat();
+        System.out.println("The area of equilateral triangle with side length "+side+" cm = "+((Math.sqrt(3)/4)*side)+" sq. cm");
+*/
+
+/*
        6. Area Of Rhombus
+        System.out.print("Enter the side length of rhombus (in cm): ");
+        float side = input.nextFloat();
+        System.out.print("Enter the height of rhombus (in cm): ");
+        float height = input.nextFloat();
+        System.out.println("The area of rhombus with base length "+side+" cm and height"+height+" cm = "+(side*height)+" sq. cm");
 */
 /*
-       7. Area Of Equilateral Triangle
        8. Perimeter Of Circle
-       9. Perimeter Of Equilateral Triangle
-       10. Perimeter Of Parallelogram
-       11. Perimeter Of Rectangle
-       12. Perimeter Of Square
-       13. Perimeter Of Rhombus
-       14. Volume Of Cone Java Program
-       15. Volume Of Prism
-       16. Volume Of Cylinder
-       17. Volume Of Sphere
-       18. Volume Of Pyramid
-       19. Curved Surface Area Of Cylinder
-       20. Total Surface Area Of Cube
-       21. Fibonacci Series In Java Programs
-       22. Subtract the Product and Sum of Digits of an Integer
-       23. Input a number and print all the factors of that number (use loops).
-       24. Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
-       25. Take integer inputs till the user enters 0 and print the largest number from all.
-       26. Addition Of Two Numbers
+        System.out.print("Enter the radius of the circle (in cm): ");
+        float r = input.nextFloat();
+        System.out.println("The perimeter of the circle of radius "+r+" cm = "+(Math.round(2*3.14*r))+" cm");
 */
+/*
+       9. Perimeter Of Equilateral Triangle
+        System.out.print("Enter the side length of equilateral triangle (in cm): ");
+        float side = input.nextFloat();
+        System.out.println("The perimeter of equilateral triangle with side length "+side+" cm = "+(3*side)+" cm");
+*/
+
+/*
+       10. Perimeter Of Parallelogram
+        System.out.print("Enter the length of parallelogram (in cm): ");
+        float length = input.nextFloat();
+        System.out.print("Enter the breadth of parallelogram (in cm): ");
+        float breadth = input.nextFloat();
+        System.out.println("The perimeter of parallelogram with length "+length+" cm and breadth"+breadth+" cm = "+(2*(breadth+length))+" cm");
+*/
+
+/*
+       11. Perimeter Of Rectangle Program
+
+        System.out.print("Enter the length of rectangle (in cm): ");
+        float length = input.nextFloat();
+        System.out.print("Enter the breadth of rectangle (in cm): ");
+        float breadth = input.nextFloat();
+        System.out.println("The perimeter of rectangle with length "+length+" cm and breadth"+breadth+" cm = "+(2*(breadth+length))+" sq. cm");
+*/
+
+/*
+       12. Perimeter Of Square
+        System.out.print("Enter the side length of square: ");
+        float side = input.nextFloat();
+        System.out.println("The perimeter of square of side length "+side+" cm = "+(side*4)+" cm");
+*/
+/*
+       13. Perimeter Of Rhombus
+        System.out.print("Enter the side length of rhombus: ");
+        float side = input.nextFloat();
+        System.out.println("The perimeter of rhombus of side length "+side+" cm = "+(side*4)+" cm");
+*/
+/*
+       14. Volume Of Cone Java Program
+        System.out.print("Enter the radius of cone (in cm): ");
+        float r = input.nextFloat();
+        System.out.print("Enter the height of cone (in cm): ");
+        float h = input.nextFloat();
+        System.out.println("The volume of cone of radius "+r+" cm and height "+h+" cm = "+((22*r*r*h)/7)+" cubic cm");
+*/
+/*
+       15. Volume Of Prism
+        System.out.print("Enter the base area of prism (in sq. cm): ");
+        float b = input.nextFloat();
+        System.out.print("Enter the height of prism (in cm): ");
+        float h = input.nextFloat();
+        System.out.println("The volume of prism of base area "+b+" sq. cm and height "+h+" cm = "+(b*h)+" cubic cm");
+*/
+
+/*
+       16. Volume Of Cylinder
+        System.out.print("Enter the radius of cylinder: ");
+        float r = input.nextFloat();
+        System.out.print("Enter the height of cylinder: ");
+        float h = input.nextFloat();
+        System.out.println("The volume of cylinder of radius "+h+" cm and radius "+r+" cm = "+((22*r*r*h)/7)+" cubic cm");
+*/
+/*
+       17. Volume Of Sphere
+
+        System.out.print("Enter the radius of sphere (in cm): ");
+        float r = input.nextFloat();
+        System.out.println("The volume of sphere of radius "+r+" cm = "+((4*22*r*r*r)/(3*7))+" cubic cm");
+*/
+/*
+       18. Volume Of Pyramid
+
+        System.out.print("Enter the base length of pyramid: ");
+        float length = input.nextFloat();
+        System.out.print("Enter the pyramid height of pyramid: ");
+        float height = input.nextFloat();
+        System.out.print("Enter the base width of pyramid: ");
+        float width = input.nextFloat();
+        System.out.println("The volume of pyramid of base length "+ length+" cm, base width "+width+" cm and pyramid height"+height+" cm = "+((length*width*height)/3)+" cubic cm");
+*/
+
+/*
+       19. Curved Surface Area Of Cylinder
+
+        System.out.print("Enter the radius of the cylinder (in cm): ");
+        float r = input.nextFloat();
+        System.out.print("Enter the height of the cylinder (in cm): ");
+        float h = input.nextFloat();
+        System.out.println("The surface area of the cylinder of radius "+r+" cm and height"+h+" cm = "+((2*22*r*h*(r+h))/7)+" sq. cm");
+*/
+/*
+       20. Total Surface Area Of Cube
+
+        System.out.print("Enter the side length of cube (in cm): ");
+        float side = input.nextFloat();
+        System.out.println("The total surface area of cube of side length "+side+" cm = "+(6*side*side)+" sq. cm");
+*/
+
+/*
+       21. Fibonacci Series In Java Programs
+        System.out.print("Enter the value of n up-to which you need value of fibonacci series: ");
+        int userN = input.nextInt();
+        int first = 0;
+        int second = 1;
+        if(userN >=1){
+            System.out.print(first + " ");
+        }
+        if (userN >= 2) {
+            System.out.print(second + " ");
+
+        }
+
+        for(int n =  3;n<=userN;n++){
+            int next = first + second;
+            System.out.print(next+" ");
+            first = second;
+            second = next;
+        }
+
+
+
+*/
+/*
+       22. Subtract the Product and Sum of Digits of an Integer - on leetcode
+*/
+
+
+/*
+       23. Input a number and print all the factors of that number (use loops).
+        System.out.print("Enter the number: ");
+        int num = input.nextInt();
+        int i = 1;
+        while(i<=num){
+            if(num%i ==0){
+                System.out.println(i);
+            }
+            i++;
+        }
+*/
+
+/*
+       24. Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+        int sum = 0;
+        while(true){
+        System.out.print("Enter the number: ");
+        int num = input.nextInt();
+            if(num ==0){
+                break;
+            }
+            else{
+                sum+=num;
+            }
+
+        }
+        System.out.println("Sum of entered numbers = "+sum);
+
+*/
+/*
+       25. Take integer inputs till the user enters 0 and print the largest number from all.
+        int largest = 0;
+        while(true){
+            System.out.print("Enter the number: ");
+            int num = input.nextInt();
+            if(num ==0){
+                break;
+            }
+            else{
+                if(num>largest){
+                    largest = num;
+                }
+            }
+
+        }
+        System.out.println("Largest of entered numbers = "+largest);
+
+*/
+/*
+       26. Addition Of Two Numbers
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+        System.out.println("The sum of 2 numbers entered = "+(num1+num2));
+*/
+/*
+    Completed Basic Java Problems Assignment
+
+*/
+
     }
 }
